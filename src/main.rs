ecfecf94e2b44c1d705caf506a51ac31873cee04
Ágata Ordano <ordanoagata@gmail.com>
2022-05-11 00:03:30 -0300

    refactor(main): reorder functions, fix types and cosmetic debug output changes

--- src/main.rs
+++ src/main.rs
@@ -5,76 +5,28 @@ use i3ipc::I3EventListener as i3event;
 use i3ipc::Subscription as event_list;
 use std::{path, process};
 
+mod functions;
 mod model;
 mod tree;
 
-fn main_loop(mut data: model::I3Data) -> Result<(), model::LoopError> {
-    let workspace = &data.events.subscribe(&[event_list::Workspace]);
-    let window = &data.events.subscribe(&[event_list::Window]);
-    let mut graph = tree::parse(&mut data);
-    println!("{:?}", graph);
-    let mut event_stream = data.events.listen();
-
-    for event in event_stream {
-        match event {
-            Ok(this_event) => match this_event {
-                i3ipc::event::Event::WorkspaceEvent(workspace_event) => {}
-                i3ipc::event::Event::WindowEvent(window_event) => {}
-                _ => {}
-            },
-            Err(error) => {}
-        }
+fn main() {
+    let matches = clap::Command::new("i3-wsnames")
+        .version("0.1.0")
+        .author("Ágata Ordano")
+        .about("Renames workspaces arbitrarily, showing what they contain without losing addressability.AsMut")
+        .arg(
+            clap::Arg::new("config")
+                .takes_value(true)
+                .forbid_empty_values(true)
+                .short('c')
+                .help("Chooses the config file path"),
+        )
+        .get_matches();
+    if let Err(error) = run(matches) {
+        println!("Application error: {}", "TODO: Error handling, sorry :(");
+        process::exit(1);
     }
-    Ok(())
 }
-
-fn setup(config_path: Option<&str>) -> Result<model::I3Data, model::SetupError> {
-    let connection;
-    let events;
-    let config_data: Option<config::Config>;
-
-    match config_path {
-        Some(path) => {
-            let config_result = read_config(path);
-            match config_result {
-                Ok(config) => {
-                    config_data = Some(config);
-                }
-                Err(error) => {
-                    println!("{:?}", error);
-                    return Err(model::SetupError::Config(error));
-                }
-            }
-        }
-        None => {
-            config_data = None;
-        }
-    }
-
-    match i3::connect() {
-        Err(error) => {
-            // TODO Fancy error handling here
-            println!("{}", error);
-            return Err(model::SetupError::I3Connection(error));
-        }
-        Ok(conn) => connection = conn,
-    }
-
-    match i3event::connect() {
-        Err(error) => {
-            // TODO Fancy error handling here
-            println!("{}", error);
-            return Err(model::SetupError::I3Connection(error));
-        }
-        Ok(evs) => events = evs,
-    }
-    Ok(model::I3Data {
-        connection,
-        events,
-        config: config_data,
-    })
-}
-
 fn run(matches: clap::ArgMatches) -> Result<(), model::I3WSNamesError> {
     let config = matches.value_of("config");
 
@@ -83,27 +35,27 @@ fn run(matches: clap::ArgMatches) -> Result<(), model::I3WSNamesError> {
             // TODO Fancy error handling here
             match error {
                 model::SetupError::I3Connection(error) => {
-                    println!("{}", error);
+                    println!("\n DEBUG: {}", error);
                     return Err(model::I3WSNamesError::Setup(
                         model::SetupError::I3Connection(error),
                     ));
                 }
                 model::SetupError::I3Message(error) => {
-                    println!("{}", error);
+                    println!("\n DEBUG: {}", error);
                     return Err(model::I3WSNamesError::Setup(model::SetupError::I3Message(
                         error,
                     )));
                 }
                 model::SetupError::Config(error) => {
-                    println!("{}", error);
+                    println!("\n DEBUG: {}", error);
                     return Err(model::I3WSNamesError::Setup(model::SetupError::Config(
                         error,
                     )));
                 }
             }
         }
-        Ok(data) => {
-            match main_loop(data) {
+        Ok(setup_data) => {
+            match main_loop(setup_data.data, setup_data.events) {
                 Err(error) => {
                     match error {
                         model::LoopError::ApplyError => {
@@ -126,39 +78,91 @@ fn run(matches: clap::ArgMatches) -> Result<(), model::I3WSNamesError> {
         }
     }
 }
+fn setup(config_path: Option<&str>) -> Result<model::SetupData, model::SetupError> {
+    let connection;
+    let events;
+    let config_data: Option<model::Config>;
 
-fn main() {
-    let matches = clap::Command::new("i3-wsnames")
-        .version("0.1.0")
-        .author("Ágata Ordano")
-        .about("Renames workspaces arbitrarily, showing what they contain without losing addressability.AsMut")
-        .arg(
-            clap::Arg::new("config")
-                .takes_value(true)
-                .forbid_empty_values(true)
-                .short('c')
-                .help("Chooses the config file path"),
-        )
-        .get_matches();
-    if let Err(error) = run(matches) {
-        println!("Application error: {}", "TODO: Error handling, sorry :(");
-        process::exit(1);
+    match config_path {
+        Some(path) => {
+            let config_result = functions::read_config(path);
+            match config_result {
+                Ok(config) => {
+                    config_data = Some(config);
+                }
+                Err(error) => {
+                    println!("\n DEBUG: {:?}", error);
+                    return Err(model::SetupError::Config(error));
+                }
+            }
+        }
+        None => {
+            config_data = None;
+        }
     }
+
+    match i3::connect() {
+        Err(error) => {
+            // TODO Fancy error handling here
+            println!("\n DEBUG: {}", error);
+            return Err(model::SetupError::I3Connection(error));
+        }
+        Ok(conn) => connection = conn,
+    }
+
+    match i3event::connect() {
+        Err(error) => {
+            // TODO Fancy error handling here
+            println!("\n DEBUG: {}", error);
+            return Err(model::SetupError::I3Connection(error));
+        }
+        Ok(evs) => events = evs,
+    }
+    Ok(model::SetupData {
+        data: model::I3Data {
+            connection,
+            config: config_data,
+        },
+        events: events,
+    })
 }
 
-pub fn read_config(config_path: &str) -> Result<config::Config, config::ConfigError> {
-    // Check if a custom path is set and build the PathBuf
-    let mut path = path::PathBuf::new();
+fn main_loop(mut data: model::I3Data, mut events: i3event) -> Result<(), model::LoopError> {
+    let workspace = &events.subscribe(&[event_list::Workspace]);
+    let window = &events.subscribe(&[event_list::Window]);
+    let mut tree = tree::parse(&mut data);
+    println!("\n[Parse Tree]\n DEBUG: {:?}", tree);
+    let mut event_stream = events.listen();
 
-    path.push(config_path);
-    path.push("i3-wsnames");
-
-    // load and return the config
-    let config = config::Config::builder()
-        .add_source(config::File::new(
-            path.to_str().unwrap(),
-            config::FileFormat::Json,
-        ))
-        .build();
-    return config;
+    for event in event_stream {
+        match event {
+            Ok(this_event) => match this_event {
+                i3ipc::event::Event::WorkspaceEvent(workspace_event) => {
+                    let mut temp_tree: Vec<model::Tree> = Vec::new();
+                    for output_tree in tree.as_ref().unwrap() {
+                        temp_tree.push(
+                            functions::do_workspace(
+                                &workspace_event,
+                                &mut data,
+                                output_tree.clone(),
+                            )
+                            .unwrap(),
+                        );
+                    }
+                }
+                i3ipc::event::Event::WindowEvent(window_event) => {
+                    let mut temp_tree: Vec<model::Tree> = Vec::new();
+                    for output_tree in tree.as_ref().unwrap() {
+                        temp_tree.push(
+                            functions::do_window(&window_event, &mut data, output_tree.clone())
+                                .unwrap(),
+                        );
+                    }
+                }
+                _ => {}
+            },
+            Err(error) => {}
+        }
+    }
+    Ok(())
 }
