63d580ba0ddf68c0c235e3e21b192a651adb6e58
Ágata Ordano <ordanoagata@gmail.com>
2022-05-11 00:02:45 -0300

    feat(model): model the config data

--- src/model.rs
+++ src/model.rs
@@ -1,20 +1,59 @@
 use i3ipc::I3Connection as i3;
 use i3ipc::I3EventListener as i3event;
+use serde::Deserialize;
 
+#[derive(Debug)]
 pub struct I3Data {
     pub connection: i3,
+    pub config: Option<Config>,
+}
+
+#[derive(Debug)]
+pub struct SetupData {
+    pub data: I3Data,
     pub events: i3event,
-    pub config: Option<config::Config>,
+}
+
+#[derive(Debug)]
+pub struct ConfigData {
+    pub rename: ConfigRename,
+    pub cross_boundary: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    rename: ConfigRename,
+    cross_boundary: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigRename {
+    pub windows: Vec<ConfigWindow>,
+    pub workspaces: Vec<ConfigWorkspace>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigWindow {
+    class: String,
+    name: String
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ConfigWorkspace {
+    number: i16,
+    name: String
 }
 
 /**
  * TODO
  */
+#[derive(Debug)]
 pub struct ApplyError {}
 
 /**
  * TODO
  */
+#[derive(Debug)]
 pub struct UpdateError {}
 
 #[derive(Debug)]
@@ -23,7 +62,7 @@ pub enum I3WSNamesError {
     Setup(SetupError),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum LoopError {
     ApplyError,
     UpdateError,
@@ -35,14 +74,14 @@ pub enum SetupError {
     I3Message(i3ipc::MessageError),
     Config(config::ConfigError),
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Tree {
     pub workspaces: Vec<Workspace>,
     pub windows: Vec<Window>,
     pub output: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Workspace {
     pub name: String,
     pub display_name: String,
@@ -52,7 +91,7 @@ pub struct Workspace {
     pub focused: bool,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Window {
     pub in_workspace: i64,
     pub i3_id: i64,
